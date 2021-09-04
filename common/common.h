138079ade78353f1ea1d3d1d9603147a16da16c8
Amir Mohammad Tavakkoli <tavakkoli.amirmohammad@gmail.com>
2021-09-04 23:19:37 +0430

    Add sum array kernel

--- common/common.h
+++ common/common.h
@@ -1,6 +1,8 @@
 #ifndef PROFESIONAL_CUDA_COMMON_H
 #define PROFESIONAL_CUDA_COMMON_H
 
+#include <sys/time.h>
+
 #define CHECK(call)                                                            \
 {                                                                              \
     const cudaError_t error = call;                                            \
@@ -10,6 +12,13 @@
         fprintf(stderr, "code: %d, reason: %s\n", error,                       \
                 cudaGetErrorString(error));                                    \
     }                                                                          \
+}
+
+
+inline double seconds() {
+    struct timeval tp;
+    gettimeofday(&tp, NULL);
+    return ((double) tp.tv_sec + (double) tp.tv_usec * 1.e-6);
 }
 
 #endif //PROFESIONAL_CUDA_COMMON_H
