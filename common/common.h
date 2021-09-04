794e808e3995a4b8615196a5c2f197614f3ed81f
Amir Mohammad Tavakkoli <tavakkoli.amirmohammad@gmail.com>
2021-09-04 23:03:43 +0430

    Add error checking macro for CUDA

---
+++ common/common.h
@@ -0,0 +1,15 @@
+#ifndef PROFESIONAL_CUDA_COMMON_H
+#define PROFESIONAL_CUDA_COMMON_H
+
+#define CHECK(call)                                                            \
+{                                                                              \
+    const cudaError_t error = call;                                            \
+    if (error != cudaSuccess)                                                  \
+    {                                                                          \
+        fprintf(stderr, "Error: %s:%d, ", __FILE__, __LINE__);                 \
+        fprintf(stderr, "code: %d, reason: %s\n", error,                       \
+                cudaGetErrorString(error));                                    \
+    }                                                                          \
+}
+
+#endif //PROFESIONAL_CUDA_COMMON_H
