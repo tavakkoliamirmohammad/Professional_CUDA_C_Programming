af06636ce68dc238d98fb0e6ba0a5173055280d5
Amir Mohammad <tavakkoli.amirmohammad@gmail.com>
2021-09-17 15:16:27 +0430

    Add check square shared memory kernels

--- Chapter05/checkSquareSharedMem.cu
+++ Chapter05/checkSquareSharedMem.cu
@@ -29,32 +29,81 @@ void printData(const char *msg, int *in,  const int size)
 
 __global__ void setRowReadRow (int *out)
 {
+   __shared__ int tile[BDIMY][BDIMX];
+
+   unsigned int idx = threadIdx.y * blockDim.x + threadIdx.x;
+
+   tile[threadIdx.y][threadIdx.x] = idx;
+   __syncthreads();
+
+   out[idx] = tile[threadIdx.y][threadIdx.x];
    
 }
 
 __global__ void setColReadCol (int *out)
 {
-  
+    __shared__ int tile[BDIMY][BDIMX];
+
+    unsigned int idx = threadIdx.y * blockDim.x + threadIdx.x;
+ 
+    tile[threadIdx.x][threadIdx.y] = idx;
+    __syncthreads();
+ 
+    out[idx] = tile[threadIdx.x][threadIdx.y];
 }
 
 __global__ void setRowReadCol(int *out)
 {
- 
+    __shared__ int tile[BDIMY][BDIMX];
+
+    unsigned int idx = threadIdx.y * blockDim.x + threadIdx.x;
+
+    tile[threadIdx.y][threadIdx.x] = idx;
+    __syncthreads();
+
+    out[idx] = tile[threadIdx.x][threadIdx.y];
 }
 
 
 __global__ void setRowReadColDyn(int *out)
 {
- 
+    extern __shared__ int tile[];
+    unsigned int rowIdx = threadIdx.y * blockDim.x + threadIdx.x;
+    unsigned int colIdx = threadIdx.x * blockDim.y + threadIdx.y;
+
+    tile[rowIdx] = rowIdx;
+    __syncthreads();
+
+    out[rowIdx] = tile[colIdx];
+    
 }
 
 __global__ void setRowReadColPad(int *out)
 {
+    __shared__ int tile[BDIMY][BDIMX + IPAD];
+
+    unsigned int idx = threadIdx.y * blockDim.x + threadIdx.x;
  
+    tile[threadIdx.x][threadIdx.y] = idx;
+    __syncthreads();
+ 
+    out[idx] = tile[threadIdx.x][threadIdx.y];
+
 }
 
 __global__ void setRowReadColDynPad(int *out)
 {
+
+    extern __shared__ int tile[];
+    unsigned int rowIdx = threadIdx.y * (blockDim.x + IPAD) + threadIdx.x;
+    unsigned int colIdx = threadIdx.x * (blockDim.x + IPAD) + threadIdx.y;
+
+    unsigned int g_idx = threadIdx.y * blockDim.x + threadIdx.x;
+
+    tile[rowIdx] = g_idx;
+    __syncthreads();
+
+    out[g_idx] = tile[colIdx];
   
 }
 
