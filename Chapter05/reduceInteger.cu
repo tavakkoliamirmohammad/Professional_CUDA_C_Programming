c37753ac727dbe63163c905c20f867c164d8a5cd
Amir Mohammad <tavakkoli.amirmohammad@gmail.com>
2021-11-02 17:32:40 +0330

    Add neighbour shared memory implementation of reducing integers

--- Chapter05/reduceInteger.cu
+++ Chapter05/reduceInteger.cu
@@ -357,7 +357,36 @@ __global__ void reduceNeighboredGmem(int *g_idata, int *g_odata,
 __global__ void reduceNeighboredSmem(int *g_idata, int *g_odata,
                                      unsigned int  n)
 {
-    g_odata[blockIdx.x] = g_idata[0];
+    __shared__ int smem[DIM];
+
+    // set thread ID
+    unsigned int tid = threadIdx.x;
+    unsigned int idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+    // convert global data pointer to the local pointer of this block
+    int *idata = g_idata + blockIdx.x * blockDim.x;
+
+    smem[tid] = idata[tid];
+    __syncthreads();
+
+
+    // boundary check
+    if (idx >= n) return;
+
+    // in-place reduction in global memory
+    for (int stride = 1; stride < blockDim.x; stride *= 2)
+    {
+        if ((tid % (2 * stride)) == 0)
+        {
+            smem[tid] += smem[tid + stride];
+        }
+
+        // synchronize within threadblock
+        __syncthreads();
+    }
+
+    // write result for this block to global mem
+    if (tid == 0) g_odata[blockIdx.x] = smem[0];
 
 }
 
